308805955fdb0d69fd7cdfd9dff5c2b5bc61641a
BinayPadhan <binaypadhan26@gmail.com>
2024-06-21 11:44:43 -0400

    message

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,39 +5,61 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// public_users.post("/register", (req,res) => {
+    
+// });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const authorName = req.params.author;
+    let bookDetails = null;
+
+    for (let bookId in books) {
+        if (books[bookId].author === authorName) {
+            bookDetails = books[bookId];
+            res.json(bookDetails);
+            break;
+        }
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let bookDetails = null;
+
+    for (let bookId in books) {
+        if (books[bookId].title === title) {
+            bookDetails = books[bookId];
+            res.json(bookDetails);
+            break;
+        }
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let bookDetails = null;
+
+    for (let bookId in books) {
+        if (bookId === isbn) {
+            bookDetails = books[bookId].reviews;
+            res.json(bookDetails);
+            break;
+        }
+    }
 });
 
 module.exports.general = public_users;
